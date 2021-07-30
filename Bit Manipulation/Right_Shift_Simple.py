a25155c789483c20e347f8eb023c7efb9ab649c0
Pallavi <86785954+Pallavi0804@users.noreply.github.com>
2021-07-30 17:47:07 +0530

    Create Right_Shift_Simple.py

---
+++ Bit Manipulation/Right_Shift_Simple.py
@@ -0,0 +1,10 @@
+def cal_right_shift(n,right_shift_times):
+    return n>>right_shift_times
+
+
+n = 5
+right_shift_times = 1
+print(cal_right_shift(n,right_shift_times))
+
+# to cross your answer apply below formula
+# formula = n/n^times
